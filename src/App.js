b30b95fd30c9ce9cd9804f4bf8a6588ba87c10ec
Shaivyaa <shukla.shaivya19991@gmail.com>
2025-02-21 22:28:53 +0530

    file name change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import Footer from "./components/Footer";
-import "./app.css";
+import "./App.css";
 
 export default function App() {
   return (
